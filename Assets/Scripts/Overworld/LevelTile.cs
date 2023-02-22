8032d3292ba84075c3c98d3d58e217b2a301d1be
Joel Stokes <joel.stokes.e@gmail.com>
2023-02-21 22:03:41 -0500

    Overworld Movement

--- Assets/Scripts/Overworld/LevelTile.cs
+++ Assets/Scripts/Overworld/LevelTile.cs
@@ -4,54 +4,103 @@ using UnityEngine;
 
 public class LevelTile : MonoBehaviour
 {
-    public int levelNumber; //Pulls level data from saved data
-    public Vector2 exit1Pos;
-    public Vector2 exit2Pos;
-    public LineRenderer exit1Line;
-    public LineRenderer exit2Line;
+    public string levelScene;
+    public Route[] routes = new Route[4];  //North, East, South, West
+    private Vector3[,] routePositions = new Vector3[4,4];  //Final values applied here to prevent reversal overwriting original
+    public bool[] reversed = new bool[4];
 
-    //Level Data
-    class pathData{
-        int exitNumber;
-        bool exitCleared;
-        
+    void Start(){
+        SetRoutePositions();
     }
 
-    private string levelName;
-    private bool exit1Clear = false;
-    private GameObject exit1Level;
-    private bool exit2Clear = false;
-    private GameObject exit2Level;
-
-    void Start()
-    {
-        exit1Line = GetComponent<LineRenderer>();
-
-        GetLevelData();
-        SetPaths();
+    private void SetRoutePositions(){
+        for (int i=0; i<4; i++){
+            if (routes[i]){
+                for (int j = 0; j<4; j++)
+                {
+                    if (routes[i].controlPoints[j]){
+                        Vector3 newPos = new Vector3(routes[i].controlPoints[j].position.x, routes[i].controlPoints[j].position.y, routes[i].controlPoints[j].position.z);
+                        if (reversed[i]){
+                            routePositions[i,Mathf.Abs(j-3)] = newPos;
+                        } else {
+                            routePositions[i,j] = newPos;
+                        }
+                    } else {
+                        routePositions[i,j] = Vector3.zero;
+                    }
+                }
+            }
+        }
     }
 
-    void Update()
-    {
-        
+    public Vector3[,] GetPositions(){
+        return routePositions;
     }
 
-    private void GetLevelData(){
-        
+    //private Route[] routes = new Route[4];  
 
-        exit1Pos = exit1Level.transform.position;
-        exit2Pos = exit2Level.transform.position;
+    /*void Start(){
+        //Add check later to see if level has been unlocked!!!!!
+        if (levelN != "" && levelN != null){
+            routes[0] = FindRoute("N", 0, levelN);
+        }
+
+        if (levelE != "" && levelE != null){
+            routes[1] = FindRoute("E", 1, levelE);
+        }
+
+        if (levelS != "" && levelS != null){
+            routes[2] = FindRoute("S", 2, levelS);
+        }
+
+        if (levelW != "" && levelW != null){
+            routes[3] = FindRoute("W", 3, levelW);
+        }
     }
 
-    private void SetPaths(){
-        if (exit1Clear){
-            
-        } else if (exit2Clear){
+    private Route FindRoute(string dir, int dirInt, string name){
+        var childObj = transform.Find(dir);
+        Debug.Log("childObj: " + childObj + ", dir: " + dir + ", dirInt: " + dirInt + ", name: " + name);
+        if (childObj != null)
+        {
+            return childObj.GetComponent<Route>();
+        } else {    //Route is from a previous level, reverse other level's route
+            Route otherRoute = GameObject.Find("/Levels/" + name).GetComponent<LevelTile>().GetRouteByName(transform.name);
+
+            if (otherRoute != null){
+                List<Transform> points = new List<Transform>();
+                for (int i=0; i<4; i++){
+                    Debug.Log(otherRoute.controlPoints[i]);
+                    points.Insert(0, otherRoute.controlPoints[i]);
+                }
+
+                for (int i=0; i<4; i++){
+                    otherRoute.controlPoints[i] = points[i];
+                }
+
+                return otherRoute;
+            } else {
+                Debug.Log("ERROR: Null Route returned from " + name + " getComponent call during GetRouteByName(" + transform.name + ")");
+
+                return otherRoute;
+            }
 
         }
     }
 
-    public void PassPaths(){
+    public Route GetRouteByName(string name){
+        if (name == levelN){
+            return routes[0];
+        } else if (name == levelE){
+            return routes[1];
+        } else if (name == levelS){
+            return routes[2];
+        } else if (name == levelW){
+            return routes[3];
+        }
 
-    }
+        Debug.Log(levelN + ", " + levelE + ", " + levelS + ", " + levelW + ", ||| Looking for: " + name);
+        Debug.Log("ERROR! No matching route found for " + name + " in level tile " + transform.name);
+        return routes[0];
+    }*/
 }
--- Assets/Scripts/Overworld/PlayerOverworld.cs
+++ Assets/Scripts/Overworld/PlayerOverworld.cs
@@ -6,25 +6,59 @@ using UnityEngine.InputSystem;
 public class PlayerOverworld : MonoBehaviour
 {
     private float deadZone = .6f;
-    private float moveSpeed = 5;
+    private bool moving = false;
 
     private float moveX;
     private float moveY;
 
-    //Movement Options set by current level tile
-    private Vector2 left;
-    private Vector2 right;
-    private Vector2 up;
-    private Vector2 down;
+    private Vector3[,] routePositions;
+    private bool routesSet = false;
 
-    void Start()
-    {
-        
-    }
+    //Bezier Curve Follower
+    private float tParam = 0;
+    private float speed = 1.5f;
+
+    private Vector2 objectPosition;
+
+    private float speedModifier;
 
     void Update()
     {
-        //What's the best way to check moves to next / previous levels?
+        if (!moving){
+            if (moveX > deadZone && routePositions[1,0] != Vector3.zero){  //East
+                StartCoroutine(GoByTheRoute(1));
+            } else if (moveX < -deadZone && routePositions[3,0] != Vector3.zero){  //West
+                StartCoroutine(GoByTheRoute(3));
+            } else if (moveY > deadZone && routePositions[0,0] != Vector3.zero){  //North
+                StartCoroutine(GoByTheRoute(0));
+            } else if (moveY < -deadZone && routePositions[2,0] != Vector3.zero){  //South
+                StartCoroutine(GoByTheRoute(2));
+            }
+        }
+    }
+
+    private IEnumerator GoByTheRoute(int routeNum)
+    {
+        moving = true;
+
+        Vector2 p0 = routePositions[routeNum,0];
+        Vector2 p1 = routePositions[routeNum,1];
+        Vector2 p2 = routePositions[routeNum,2];
+        Vector2 p3 = routePositions[routeNum,3];
+
+        while(tParam < 1)
+        {
+            tParam += Time.deltaTime * speed;
+
+            objectPosition = Mathf.Pow(1 - tParam, 3) * p0 + 3 * Mathf.Pow(1 - tParam, 2) * tParam * p1 + 3 * (1 - tParam) * Mathf.Pow(tParam, 2) * p2 + Mathf.Pow(tParam, 3) * p3;
+
+            transform.position = objectPosition;
+            yield return new WaitForEndOfFrame();
+        }
+
+        tParam = 0f;
+        moving = false;
+
     }
 
     public void MoveX(InputAction.CallbackContext context){
@@ -35,7 +69,24 @@ public class PlayerOverworld : MonoBehaviour
         moveY = context.ReadValue<float>();
     }
 
+    public void Confirm(InputAction.CallbackContext context){
+        if (!moving){
+            //Start Level
+        }
+    }
+
     void OnTriggerEnter2D(Collider2D other) {
-        
+        if (other.gameObject.tag == "Level"){        
+            routePositions = other.GetComponent<LevelTile>().GetPositions();
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D other) {
+        if (!routesSet){
+            if (other.gameObject.tag == "Level"){        
+                routePositions = other.GetComponent<LevelTile>().GetPositions();
+                routesSet = true;
+            }
+        }
     }
 }
